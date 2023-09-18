91879389449ff3c3472f10467127c8667c7019a4
bayushi-yakusoku <bayushi.tonio@gmail.com>
2023-09-18 23:32:10 +0200

    Instatiate kitchen object and move them https://youtu.be/AmGSEH7QcDg?t=13057

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,13 +7,41 @@ public class ClearCounter : MonoBehaviour {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
 
-    public void Interact() {
-        Debug.Log(this + ": Interact");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+    [SerializeField] private ClearCounter destinationCounter;
+    [SerializeField] bool Testing;
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>()
-            .GetKitchenObjectSO()
-            .objectName);
+    private KitchenObject _presentedObject;
+
+    public void Interact() {
+        if (_presentedObject is null) {
+            Debug.Log(this + ": Interact");
+
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+
+            kitchenObjectTransform.GetComponent<KitchenObject>().OwnerCounter = this;
+
+            Debug.Log(_presentedObject.GetKitchenObjectSO().objectName);
+        }
+        else {
+            Debug.Log("OwnerCounter: " + _presentedObject.OwnerCounter);
+
+            if (Testing && destinationCounter) {
+                Debug.Log("Moving object...");
+                _presentedObject.OwnerCounter = destinationCounter;
+            }
+        }
     }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    public KitchenObject PresentedObject {
+        get => _presentedObject;
+
+        set {
+            _presentedObject = value;
+        }
+    }
+
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KitchenObject : MonoBehaviour
-{
+public class KitchenObject : MonoBehaviour {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    private ClearCounter _ownerCounter;
 
     public KitchenObjectSO GetKitchenObjectSO() {
         return kitchenObjectSO;
+    }
+
+    public ClearCounter OwnerCounter {
+        get => _ownerCounter;
+
+        set {
+            if (_ownerCounter) {
+                _ownerCounter.PresentedObject = null;
+            }
+
+            _ownerCounter = value;
+
+            if (_ownerCounter.PresentedObject) {
+                Debug.LogError(_ownerCounter + " has already a Presented object!");
+            }
+
+            _ownerCounter.PresentedObject = this;
+
+            transform.parent = _ownerCounter.GetKitchenObjectFollowTransform();
+            transform.localPosition = Vector3.zero;
+        }
     }
 }
