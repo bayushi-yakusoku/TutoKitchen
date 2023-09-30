953e2d2255e98dfff075abf0212addd3b5e12f80
bayushi-yakusoku <bayushi.tonio@gmail.com>
2023-09-30 19:33:44 +0200

    Add cut counter and animation

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,10 @@ using UnityEngine;
 public class CuttingCounter : BaseCounter {
 
     [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
+
+    private int currentCutCount = 0;
+
+    public event EventHandler OnPlayerInteractAlternateCuttingCounter;
 
     public override void Interact(Player player) {
         Debug.Log(this + ": Interact");
@@ -18,6 +23,8 @@ public class CuttingCounter : BaseCounter {
                 if (HasRecipeWithInput(player.GetPresentedObject().GetKitchenObjectSO())) {
                     Debug.Log(this + ": Recipe found");
                     player.GetPresentedObject().Owner = this;
+
+                    currentCutCount = 0;
                 }
                 else {
                     Debug.Log(this + ": NO recipe found");
@@ -31,6 +38,8 @@ public class CuttingCounter : BaseCounter {
                 Debug.Log(this + ": Counter has an object");
 
                 GetPresentedObject().Owner = player;
+
+                currentCutCount = 0;
             }
 
         }
@@ -42,35 +51,58 @@ public class CuttingCounter : BaseCounter {
         if (HasPresentedObject()) {
             Debug.Log(this + ": Counter has an object");
 
-            KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(
-                    GetPresentedObject().GetKitchenObjectSO()
-                );
+            CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(GetPresentedObject()
+                                                                            .GetKitchenObjectSO()
+                                                                         );
 
-            if (outputKitchenObjectSO is not null) {
-                GetPresentedObject().DestroySelf();
-                KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+            if (cuttingRecipeSO is null) {
+                Debug.Log(this + ": no cutting recipe found");
+
+                return;
+            }
+
+            currentCutCount++;
+
+            OnPlayerInteractAlternateCuttingCounter.Invoke(this, EventArgs.Empty);
+
+            if (currentCutCount >= cuttingRecipeSO.CutCountNeeded) {
+                KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(GetPresentedObject()
+                                                                            .GetKitchenObjectSO()
+                                                                         );
+
+                if (outputKitchenObjectSO is not null) {
+                    GetPresentedObject().DestroySelf();
+                    KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+                }
+
+                currentCutCount = 0;
             }
         }
     }
 
     private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO) {
-        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray) {
-            if (cuttingRecipeSO.input == inputKitchenObjectSO) {
-                return true;
-            }
-        }
+        CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(inputKitchenObjectSO);
 
-        return false;
-
+        return cuttingRecipeSO != null;
     }
 
     private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO) {
+        CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeSOWithInput(inputKitchenObjectSO);
+
+        if (cuttingRecipeSO is not null) {
+            return cuttingRecipeSO.output;
+        }
+
+        return null;
+    }
+
+    private CuttingRecipeSO GetCuttingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO) {
         foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray) {
             if (cuttingRecipeSO.input == inputKitchenObjectSO) {
-                Debug.Log(this + ": input is " + cuttingRecipeSO.input +
+                Debug.Log(this + ": found recipe, input is " + cuttingRecipeSO.input +
                     " output is " + cuttingRecipeSO.output);
 
-                return cuttingRecipeSO.output;
+                return cuttingRecipeSO;
             }
         }
 
---
+++ Assets/Scripts/CuttingCounterVisual.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingCounterVisual : MonoBehaviour
+{
+    private const string CUT = "Cut";
+    private int hashCut;
+
+    [SerializeField] private CuttingCounter cuttingCounter;
+
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        hashCut = Animator.StringToHash(CUT);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cuttingCounter.OnPlayerInteractAlternateCuttingCounter += CuttingCounter_OnPlayerInteractAlternateCuttingCounter;
+    }
+
+    private void CuttingCounter_OnPlayerInteractAlternateCuttingCounter(object sender, System.EventArgs e) {
+        animator.SetBool(hashCut, true);
+    }
+}
