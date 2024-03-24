16600eb6cd0f0698f6945345b454680878189061
bayushi-yakusoku <bayushi.tonio@gmail.com>
2024-03-24 16:26:14 +0100

    Change delivery feedback

--- Assets/Scripts/UI/DeliveryCounterFeedbackUI.cs
+++ Assets/Scripts/UI/DeliveryCounterFeedbackUI.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class DeliveryCounterFeedbackUI : MonoBehaviour {
 
+    private const string SUCCESS_RESULT = "Delivery\nSuccess";
+    private const string FAILED_RESULT = "Delivery\nFailed";
+
     [SerializeField] private Image background;
     [SerializeField] private Color successBackgroundColor;
     [SerializeField] private Color failedBackgroundColor;
@@ -13,14 +16,46 @@ public class DeliveryCounterFeedbackUI : MonoBehaviour {
     [Space(10)]
 
     [SerializeField] private Image feedbackImage;
-    [SerializeField] private Texture successImageTexture;
+    [SerializeField] private Sprite successImageSprite;
     [SerializeField] private Color successImageColor;
-    [SerializeField] private Texture failedImageTexture;
+    [SerializeField] private Sprite failedImageSprite;
     [SerializeField] private Color failedImageColor;
 
     [Space(10)]
 
     [SerializeField] private TextMeshProUGUI feedbackText;
 
+
+    private void Start() {
+        DeliveryManager.Instance.OnDeliverySuccess += DeliveryManager_OnDeliverySuccess;
+        DeliveryManager.Instance.OnDeliveryFailed += DeliveryManager_OnDeliveryFailed;
+    }
+
+    private void DeliveryManager_OnDeliveryFailed(object sender, System.EventArgs e) {
+        DisplayFailed();
+    }
+
+    private void DeliveryManager_OnDeliverySuccess(object sender, System.EventArgs e) {
+        DisplaySuccess();
+    }
+
+
+    private void DisplaySuccess() {
+        background.color = successBackgroundColor;
+
+        feedbackImage.sprite = successImageSprite;
+        feedbackImage.color = successImageColor;
+
+        feedbackText.text = SUCCESS_RESULT;
+    }
+
+    private void DisplayFailed() {
+        background.color = failedBackgroundColor;
+
+        feedbackImage.sprite = failedImageSprite;
+        feedbackImage.color = failedImageColor;
+
+        feedbackText.text = FAILED_RESULT;
+    }
 
 }
